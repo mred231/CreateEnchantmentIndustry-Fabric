e5a52fbe287c19aa4570d7560892b4e61187afde
MarbleGate <marblegatekeeper@outlook.com>
2023-06-28 08:08:06 +0800

    Fix Mob don't drop experience nuggets while killed by deployer (#6), Close #6

--- src/main/java/plus/dragons/createenchantmentindustry/foundation/mixin/DeployerFakePlayerMixin.java
+++ src/main/java/plus/dragons/createenchantmentindustry/foundation/mixin/DeployerFakePlayerMixin.java
@@ -5,10 +5,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.simibubi.create.AllItems;
 import com.simibubi.create.content.kinetics.deployer.DeployerFakePlayer;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import plus.dragons.createenchantmentindustry.foundation.config.CeiConfigs;
 
 
 @Mixin(value = DeployerFakePlayer.class, remap = false)
@@ -16,8 +20,22 @@ public class DeployerFakePlayerMixin {
 
     @Inject(method = "deployerKillsDoNotSpawnXP", at = @At(value = "HEAD"), cancellable = true)
     private static void deployerKillsSpawnXpNuggets(int i, Player player, LivingEntity entity, CallbackInfoReturnable<Integer> cir) {
-		if (player instanceof DeployerFakePlayer){
-			cir.setReturnValue(i);
+		if (player instanceof DeployerFakePlayer FakePlayer){
+			int xp = i;
+			if (xp <= 0) return;
+			if (FakePlayer.getRandom().nextFloat() > CeiConfigs.SERVER.deployerXpDropChance.getF())
+				return;
+			int amount = xp / 3 + (FakePlayer.getRandom().nextInt(3) < xp % 3 ? 1 : 0);
+			if (amount <= 0) return;
+			Item nugget = AllItems.EXP_NUGGET.get();
+			int maxStackSize = nugget.getMaxStackSize();
+			for (int j = amount / maxStackSize; j > 0; --j) {
+				FakePlayer.getInventory().placeItemBackInInventory(new ItemStack(nugget, maxStackSize));
+			}
+			amount %= maxStackSize;
+			if (amount > 0)
+				FakePlayer.getInventory().placeItemBackInInventory(new ItemStack(nugget, amount));
+			cir.setReturnValue(0);
 		}
     }
 
