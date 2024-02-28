0c38e7cca4f8c6bd678dc1896dcd4155dec4e619
IThundxr <me@ithundxr.dev>
2024-02-28 15:55:25 -0500

    hide flowing fluids from jei

--- src/main/java/plus/dragons/createenchantmentindustry/compat/jei/CeiJEIPlugin.java
+++ src/main/java/plus/dragons/createenchantmentindustry/compat/jei/CeiJEIPlugin.java
@@ -5,16 +5,19 @@ import java.util.List;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import com.simibubi.create.AllFluids;
 import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.fabric.constants.FabricTypes;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IJeiRuntime;
+import mezz.jei.fabric.ingredients.fluid.JeiFluidIngredient;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Recipe;
@@ -23,6 +26,7 @@ import plus.dragons.createenchantmentindustry.compat.jei.category.DisenchantingC
 import plus.dragons.createenchantmentindustry.compat.jei.category.RecipeCategoryBuilder;
 import plus.dragons.createenchantmentindustry.content.contraptions.enchanting.disenchanter.DisenchantRecipe;
 import plus.dragons.createenchantmentindustry.entry.CeiBlocks;
+import plus.dragons.createenchantmentindustry.entry.CeiFluids;
 import plus.dragons.createenchantmentindustry.entry.CeiRecipeTypes;
 
 // @JeiPlugin Annotation is for forge so check mod.json for the entrypoint
@@ -42,7 +46,7 @@ public class CeiJEIPlugin implements IModPlugin {
 
 	@Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
-        loadCategories(registration);
+        loadCategories();
         registration.addRecipeCategories(allCategories.toArray(IRecipeCategory[]::new));
     }
 
@@ -50,6 +54,16 @@ public class CeiJEIPlugin implements IModPlugin {
     public void registerRecipes(IRecipeRegistration registration) {
 		ingredientManager = registration.getIngredientManager();
 		allCategories.forEach(c -> c.registerRecipes(registration));
+
+		// fabric: hide flowing fluids
+		registration.getIngredientManager().removeIngredientsAtRuntime(
+				FabricTypes.FLUID_STACK,
+				List.of(
+						new JeiFluidIngredient(CeiFluids.EXPERIENCE.get().getFlowing(), 1),
+						new JeiFluidIngredient(CeiFluids.HYPER_EXPERIENCE.get().getFlowing(), 1),
+						new JeiFluidIngredient(CeiFluids.INK.get().getFlowing(), 1)
+				)
+		);
     }
 
     @Override
@@ -57,20 +71,11 @@ public class CeiJEIPlugin implements IModPlugin {
         allCategories.forEach(c -> c.registerCatalysts(registration));
     }
 
-
-    @Override
-    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
-//		List<FluidStack> fluidIngredients = new ArrayList<>();
-//		fluidIngredients.add(new FluidStack(CeiFluids.EXPERIENCE.get().getSource(), 81000));
-//		fluidIngredients.add(new FluidStack(CeiFluids.HYPER_EXPERIENCE.get().getSource(), 81000));
-//		jeiRuntime.getIngredientManager().addIngredientsAtRuntime(FabricTypes.FLUID_STACK, fluidIngredients);
-    }
-
     private static <T extends Recipe<?>> RecipeCategoryBuilder<T> builder(Class<T> cls) {
         return new RecipeCategoryBuilder<>(EnchantmentIndustry.ID, cls);
     }
 
-    private void loadCategories(IRecipeCategoryRegistration registration) {
+    private void loadCategories() {
         allCategories.clear();
         allCategories.add(
                 builder(DisenchantRecipe.class)
